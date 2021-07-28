b67ccf5f73ca093dea8ce35af42e6c9e3351ebb9
zhukovgreen <iam@zhukovgreen.pro>
2021-07-28 14:56:35 +0200

    Use beakerlib for tests

--- tests/core/environment-file/test.sh
+++ tests/core/environment-file/test.sh
@@ -3,7 +3,23 @@
 . /usr/share/beakerlib/beakerlib.sh || exit 1
 
 rlJournalStart
-    rlPhaseStartTest "Run pytest session"
-        rlRun "python3 -m pytest -svv"
+    rlPhaseStartSetup
+        rlRun 'pushd data'
+        rlRun 'set -o pipefail'
+    rlPhaseEnd
+
+    rlPhaseStartTest "Check environment-file option reads properly"
+        rlRun "tmt run -vvvddd | tee output"
+        rlAssertGrep "total: 1 test passed" 'output'
+    rlPhaseEnd
+
+    rlPhaseStartTest "Check if --environment overwrites --environment-file"
+        rlRun "tmt run --environment STR=bad_str -vvvddd 2>&1 | tee output" 1
+        rlAssertGrep "AssertionError: assert 'bad_str' == 'O'" 'output'
+    rlPhaseEnd
+
+    rlPhaseStartTest "Check if cli environment-file overwrites fmf"
+        rlRun "tmt run --environment-file env-via-cli -vvvddd 2>&1 | tee output" 1
+        rlAssertGrep "AssertionError: assert '2' == '1'" 'output'
     rlPhaseEnd
 rlJournalEnd
